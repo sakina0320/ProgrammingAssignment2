b46bada2e97f880048f0d42b7bf6881b429d8172
Unknown <sakina0320@gmail.com>
2019-02-03 12:37:16 -0500

    Read some helpful instructions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Week 3 assignment: Caching matrix inverses
 
-## Write a short comment describing this function
-
+# Helpful resources:
+# https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
+# https://www.coursera.org/learn/r-programming/discussions/weeks/3/threads/ePlO1eMdEeahzg7_4P4Vvg
+# This function chaches inverted matrices.
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
-
+# This function calculates inverses.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+# Test matrix
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
